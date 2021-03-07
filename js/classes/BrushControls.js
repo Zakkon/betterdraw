88ea15004b8cfb1df8c0fa155fda5a1350ff676e
Zakkon <zakkonzoom@gmail.com>
2021-03-07 15:20:11 +0100

    added a very simple color palette

--- js/classes/BrushControls.js
+++ js/classes/BrushControls.js
@@ -68,7 +68,20 @@ export default class BrushControls extends FormApplication {
     let x = $("#select_brushsize"); x[0].innerHTML="";
     for(let i = 0; i < sizes.length; ++i) { $("<option>"+sizes[i]+"</a>").appendTo(x); }
     x.value = sizes[0];
-  } 
+
+    $("#color-palette-expand-btn").on("click", () => this.togglePaletteExpanded());
+    this.definePresets();
+    this.setPaletteExpanded(false);
+  }
+  togglePaletteExpanded(){
+    this.setPaletteExpanded($("#color-palette-grid").hasClass("hide"));
+  }
+  setPaletteExpanded(expanded){
+    let grid = $("#color-palette-grid");
+    if((expanded && grid.hasClass("hide")) || (!expanded && !grid.hasClass("hide"))){grid.toggleClass("hide");}
+    let btn = $("#color-palette-expand-btn")[0];
+    btn.innerHTML = expanded? "⯇" : "⯈";
+  }
   configureElements(activeTool) {
   }
 
@@ -103,8 +116,37 @@ export default class BrushControls extends FormApplication {
 
   refreshColor(webColor){
     let s = $("#brushColorPicker")[0];
-    console.log(s);
-    console.log(webColor);
     s.value = webColor;
+  }
+
+  definePresets(){
+    var presets = [
+      "000000", "434343", "666666", "999999", "b7b7b7", "cccccc", "d9d9d9", "efefef", "f3f3f3", "ffffff",
+      "980000", "ff0000", "ff9900", "ffff00", "00ff00", "00ffff", "fa86e8", "0000ff", "9900ff", "ff00ff",
+      "e6b8af", "f4cccc", "fce5cd", "fff2cc", "d9ead3", "d0e0e3", "c9daf8", "cfe2f3", "d9d2e9", "ead1dc",
+      "dd7e6b", "ea9999", "f9cb9c", "ffe599", "b6d7a8", "a2c4c9", "a4c2f4", "9fc5e8", "b4a7d6", "d5a6bd",
+      "cc4125", "e06666", "f6b26b", "ffd966", "93c47d", "76a5af", "6d9eeb", "6fa8dc", "8e7cc3", "c27ba0",
+      "a61c00", "cc0000", "e69138", "f1c232", "6aa84f", "45818e", "3c78d8", "3d85c6", "674ea7", "a64d79",
+      "85200c", "990000", "b45f06", "bf9000", "38761d", "134f5c", "1155cc", "0b5394", "251c75", "741b47",
+      "5b0f00", "660000", "783f04", "7f6000", "274e13", "0c343d", "1c4587", "073763", "20124d", "4c1130"];
+      presets.forEach(e => {
+        this.addPresetButton("#" + e);
+      });
+  }
+  addPresetButton(webColor){
+    let grid = $("#color-palette-grid");
+    let btn = $("<button class='color-palette-button' colorval='"+webColor+"' style='background-color: "+webColor+";'>"+"</button>").appendTo(grid);
+    btn.on("click", () => this.onColorPresetButton(btn[0]));
+  }
+  /**
+   * 
+   * @param {HTMLButtonElement} button 
+   */
+  onColorPresetButton(button){
+    console.log(button);
+    const webColor = button.getAttribute("colorval");
+    console.log(webColor);
+    setUserSetting('brushColor', webColor);
+    this.refreshColor(webColor);
   }
 }
